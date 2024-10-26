51364d7045462a77024a04a0622929ceb428d9ab
RafaelNoboru <rafael.noboru@gmail.com>
2024-10-26 17:24:20 -0300

    rabbitMQ e envio de email

--- src/main/java/br/com/fiap/iafuture/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/iafuture/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package br.com.fiap.iafuture.config;
 
+import br.com.fiap.iafuture.user.CustomUserDetailsService;
+import br.com.fiap.iafuture.user.UserRepository;
 import br.com.fiap.iafuture.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,12 +17,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/error", "/oauth2/**", "/reviews/new", "/reviews").permitAll()
+                        .requestMatchers("/login", "/register", "/error", "/oauth2/**", "/reviews/new", "/reviews", "/email/send-test").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(login -> login
                         .loginPage("/login")
